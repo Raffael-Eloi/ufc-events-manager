5c25ab6e2459d363a48f369f4d814d3c4824c022
Raffael Eloi <raffaeleloi121@gmail.com>
2026-07-20 17:37:15 -0300

    GivenRequest_WhenThereAreNewEvents_ThenTheEventsShouldBeSentToCalendar (GREEN)

--- src/UFC.Events.Manager.API/Features/UFCEvents/EventsCoordinator/UfcEventsCoordinator.cs
+++ src/UFC.Events.Manager.API/Features/UFCEvents/EventsCoordinator/UfcEventsCoordinator.cs
@@ -1,5 +1,7 @@
 using UFC.Events.Manager.API.Entities;
+using UFC.Events.Manager.API.Features.Emails.GetSubscribers;
 using UFC.Events.Manager.API.Features.UFCEvents.CreateEvents;
+using UFC.Events.Manager.API.Features.UFCEvents.EventsSender;
 using UFC.Events.Manager.API.Features.UFCEvents.GetEvents;
 
 namespace UFC.Events.Manager.API.Features.UFCEvents.EventsCoordinator;
@@ -7,21 +9,29 @@ namespace UFC.Events.Manager.API.Features.UFCEvents.EventsCoordinator;
 public class UfcEventsCoordinator : IUfcEventsCoordinator
 {
     private readonly ICreateUfcEvents _createUfcEvents;
-    
     private readonly IGetUfcEvents _getUfcEvents;
+    private readonly IGetSubscribers _getSubscribers;
+    private readonly IUfcEventsSender _ufcEventsSender;
 
-    public UfcEventsCoordinator(ICreateUfcEvents createUfcEvents, IGetUfcEvents getUfcEvents)
+    public UfcEventsCoordinator(
+        ICreateUfcEvents createUfcEvents, 
+        IGetUfcEvents getUfcEvents, 
+        IGetSubscribers getSubscribers, 
+        IUfcEventsSender ufcEventsSender)
     {
         _createUfcEvents = createUfcEvents;
         _getUfcEvents = getUfcEvents;
+        _getSubscribers = getSubscribers;
+        _ufcEventsSender = ufcEventsSender;
     }
     
     public async Task ExecuteAsync(IEnumerable<UFCEvent> events)
     {
         IDictionary<string, UFCEvent> existingEventsByName = await GetExistingEvents();
         List<UFCEvent> newEvents = events.Where(ufcEvent => !existingEventsByName.ContainsKey(ufcEvent.Name)).ToList();
-        
         await _createUfcEvents.ExecuteAsync(newEvents);
+        IEnumerable<Subscriber> subscribers = await _getSubscribers.ExecuteAsync();
+        await _ufcEventsSender.ExecuteAsync(newEvents, subscribers);
     }
 
     private async Task<IDictionary<string, UFCEvent>> GetExistingEvents()
--- tests/UFC.Events.Manager.API.Tests/Features/UFCEvents/UfcEventsCoordinatorTest.cs
+++ tests/UFC.Events.Manager.API.Tests/Features/UFCEvents/UfcEventsCoordinatorTest.cs
@@ -1,8 +1,10 @@
 using Moq;
 using UFC.Events.Manager.API.Entities;
 using UFC.Events.Manager.API.Enums;
+using UFC.Events.Manager.API.Features.Emails.GetSubscribers;
 using UFC.Events.Manager.API.Features.UFCEvents.CreateEvents;
 using UFC.Events.Manager.API.Features.UFCEvents.EventsCoordinator;
+using UFC.Events.Manager.API.Features.UFCEvents.EventsSender;
 using UFC.Events.Manager.API.Features.UFCEvents.GetEvents;
 
 namespace UFC.Events.Manager.API.Tests.Features.UFCEvents;
@@ -11,9 +13,13 @@ internal class UfcEventsCoordinatorTest
 {
     private Mock<ICreateUfcEvents> _createUfcEventsMock;
     private Mock<IGetUfcEvents> _getUfcEventsMock;
+    private Mock<IGetSubscribers> _getSubscribersMock;
+    private Mock<IUfcEventsSender> _ufcEventsSenderMock;
     private IUfcEventsCoordinator _ufcEventsCoordinator;
     private UFCEvent event1;
     private UFCEvent event2;
+    private Subscriber subscriber1;
+    private Subscriber subscriber2;
 
     [SetUp]
     public void Setup()
@@ -21,8 +27,16 @@ internal class UfcEventsCoordinatorTest
         _createUfcEventsMock = new Mock<ICreateUfcEvents>();
         
         _getUfcEventsMock = new Mock<IGetUfcEvents>();
+        
+        _getSubscribersMock = new Mock<IGetSubscribers>();
+        
+        _ufcEventsSenderMock = new Mock<IUfcEventsSender>();
 
-        _ufcEventsCoordinator = new UfcEventsCoordinator(_createUfcEventsMock.Object, _getUfcEventsMock.Object);
+        _ufcEventsCoordinator = new UfcEventsCoordinator(
+            _createUfcEventsMock.Object,
+            _getUfcEventsMock.Object,
+            _getSubscribersMock.Object,
+            _ufcEventsSenderMock.Object);
         
         event1 = new UFCEvent()
         {
@@ -50,6 +64,18 @@ internal class UfcEventsCoordinatorTest
             PreliminaryCardStartTime = new DateTime(2026, 07, 18, 18, 0, 0, DateTimeKind.Utc),
             MainCardStartTime = new DateTime(2026, 07, 18, 21, 0, 0, DateTimeKind.Utc)
         };
+        
+        subscriber1 = new Subscriber
+        {
+            Id = 1,
+            Email = "johndoe@gmail.com"
+        };
+        
+        subscriber2 = new Subscriber
+        {
+            Id = 2,
+            Email = "jane@gmail.com"
+        };
     }
 
     [Test]
@@ -64,6 +90,27 @@ internal class UfcEventsCoordinatorTest
         // Assert
         _createUfcEventsMock
             .Verify(useCase => useCase.ExecuteAsync(events),
+            Times.Once);
+    }
+    
+    [Test]
+    public async Task GivenRequest_WhenThereAreNewEvents_ThenTheEventsShouldBeSentToCalendar()
+    {
+        // Arrange
+        List<UFCEvent> events = [event1, event2];
+        
+        List<Subscriber> subscribers = [subscriber1, subscriber2];
+        
+        _getSubscribersMock
+            .Setup(useCase => useCase.ExecuteAsync())
+            .ReturnsAsync(subscribers);
+        
+        // Act
+        await _ufcEventsCoordinator.ExecuteAsync(events);
+
+        // Assert
+        _ufcEventsSenderMock
+            .Verify(useCase => useCase.ExecuteAsync(events, subscribers),
             Times.Once);
     }
     
